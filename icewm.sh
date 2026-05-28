6087eaafcc30407713760ffb4a308b69d78ade77
GabrielSantana1996sp <gabrielsantanati@outlook.com>
2026-05-28 07:57:28 -0300

    Modify setup-desktop script for XFCE environment

--- icewm.sh
+++
@@ -1,9 +0,0 @@
-nano /etc/apk/repositories
-https://dl-cdn.alpinelinux.org/alpine/v3.23/main
-https://dl-cdn.alpinelinux.org/alpine/v3.23/community
-https://dl-cdn.alpinelinux.org/alpine/edge/testing
-apk update
-apk add xorg-server xf86-video-vesa mesa
-apk add icewm
-exec icewm-session
-startx
